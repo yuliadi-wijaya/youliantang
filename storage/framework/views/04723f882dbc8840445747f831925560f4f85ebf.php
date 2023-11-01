2ce6415dc57364321d6276347ca4740991ce7516
febryana0219 <febryana0219@gmail.com>
2023-11-01 23:04:10 +0700

    fix print invoice

--- storage/framework/views/04723f882dbc8840445747f831925560f4f85ebf.php
+++ storage/framework/views/04723f882dbc8840445747f831925560f4f85ebf.php
@@ -1,10 +1,9 @@
-@extends('layouts.master-layouts')
-@section('title') {{ __('Invoice Details') }} @endsection
-@section('body')
+<?php $__env->startSection('title'); ?> <?php echo e(__('Invoice Details')); ?> <?php $__env->stopSection(); ?>
+<?php $__env->startSection('body'); ?>
 
     <body data-topbar="dark" data-layout="horizontal">
-    @endsection
-    @section('content')
+    <?php $__env->stopSection(); ?>
+    <?php $__env->startSection('content'); ?>
         <!-- Adjust the CSS for the specific paper dimensions and max diameter -->
         <style>
             @page {
@@ -56,18 +55,19 @@
         </style>
 
         <!-- start page title -->
-        @component('components.breadcrumb')
-            @slot('title') Invoice Details @endslot
-            @slot('li_1') Dashboard @endslot
-            @slot('li_2') Invoice List @endslot
-            @slot('li_3') Invoice Details @endslot
-        @endcomponent
+        <?php $__env->startComponent('components.breadcrumb'); ?>
+            <?php $__env->slot('title'); ?> Invoice Details <?php $__env->endSlot(); ?>
+            <?php $__env->slot('li_1'); ?> Dashboard <?php $__env->endSlot(); ?>
+            <?php $__env->slot('li_2'); ?> Invoice List <?php $__env->endSlot(); ?>
+            <?php $__env->slot('li_3'); ?> Invoice Details <?php $__env->endSlot(); ?>
+        <?php echo $__env->renderComponent(); ?>
         <!-- end page title -->
         <div class="row d-print-none">
             <div class="col-12">
-                <a href="{{ url('invoice') }}">
+                <a href="<?php echo e(url('invoice')); ?>">
                     <button type="button" class="btn btn-primary waves-effect waves-light mb-4">
-                        <i class="bx bx-arrow-back font-size-16 align-middle mr-2"></i>{{ __('Back to Invoice List') }}
+                        <i class="bx bx-arrow-back font-size-16 align-middle mr-2"></i><?php echo e(__('Back to Invoice List')); ?>
+
                     </button>
                 </a>
                 <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light mb-4">
@@ -81,7 +81,7 @@
                     <div class="card-body">
                         <section class="sheet">
                             <div class="invoice-title">
-                                <h4 class="float-right font-size-16">{{ __('Invoice #') }} {{ $invoice_detail->id }}</h4>
+                                <h4 class="float-right font-size-16"><?php echo e(__('Invoice #')); ?> <?php echo e($invoice_detail->id); ?></h4>
                                 <div class="mb-4">
                                     <h3>YOU LIAN tANG</h3>
                                     <h6>Family Refleksi & Massage</h6>
@@ -91,9 +91,9 @@
                             <div class="row">
                                 <div class="col-3">
                                     <address>
-                                        <strong>{{ __('Treatment date : ') }}</strong>{{ $invoice->treatment_date . ' ' . $invoice->treatment_time_from . ' to ' . $invoice->treatment_time_to }}<br>
-                                        <strong>{{ __('Customer : ') }}</strong>{{ $invoice_detail->customer_name }}<br>
-                                        <strong>{{ __('Therapist : ') }}</strong>{{ $invoice_detail->therapist_name }}<br>
+                                        <strong><?php echo e(__('Treatment date : ')); ?></strong><?php echo e($invoice->treatment_date . ' ' . $invoice->treatment_time_from . ' to ' . $invoice->treatment_time_to); ?><br>
+                                        <strong><?php echo e(__('Customer : ')); ?></strong><?php echo e($invoice_detail->customer_name); ?><br>
+                                        <strong><?php echo e(__('Therapist : ')); ?></strong><?php echo e($invoice_detail->therapist_name); ?><br>
                                     </address>
                                 </div>
                                 <div class="col-3">
@@ -108,49 +108,49 @@
                                 </div>
                                 <div class="col-3 pull-right">
                                     <address>
-                                        <strong>{{ __('Invoice date : ') }}</strong>{{ $invoice->created_at }}<br>
-                                        <strong>{{ __('Payment Mode : ') }}</strong>{{ $invoice_detail->payment_mode }}<br>
-                                        <strong>{{ __('Payment Status : ') }}</strong>{{ $invoice_detail->payment_status }}<br>
+                                        <strong><?php echo e(__('Invoice date : ')); ?></strong><?php echo e($invoice->created_at); ?><br>
+                                        <strong><?php echo e(__('Payment Mode : ')); ?></strong><?php echo e($invoice_detail->payment_mode); ?><br>
+                                        <strong><?php echo e(__('Payment Status : ')); ?></strong><?php echo e($invoice_detail->payment_status); ?><br>
                                     </address>
                                 </div>
                             </div>
 
                             <div class="py-2 mt-3">
-                                <h3 class="font-size-15 font-weight-bold">{{ __('Invoice summary') }}</h3>
+                                <h3 class="font-size-15 font-weight-bold"><?php echo e(__('Invoice summary')); ?></h3>
                             </div>
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th style="width: 70px;">{{ __('No.') }}</th>
-                                            <th>{{ __('Title') }}</th>
-                                            <th class="text-right">{{ __('Amount') }}</th>
+                                            <th style="width: 70px;"><?php echo e(__('No.')); ?></th>
+                                            <th><?php echo e(__('Title')); ?></th>
+                                            <th class="text-right"><?php echo e(__('Amount')); ?></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @php
+                                        <?php
                                             $sub_total = 0;
-                                        @endphp
-                                        @foreach ($invoice_detail->invoice_detail as $item)
+                                        ?>
+                                        <?php $__currentLoopData = $invoice_detail->invoice_detail; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                             <tr>
-                                                <td>{{ $loop->index + 1 }}</td>
-                                                <td>{{ $item->title }}</td>
-                                                <td class="text-right">Rp {{ number_format($item->amount) }}</td>
+                                                <td><?php echo e($loop->index + 1); ?></td>
+                                                <td><?php echo e($item->title); ?></td>
+                                                <td class="text-right">Rp <?php echo e(number_format($item->amount)); ?></td>
                                             </tr>
-                                            @php
+                                            <?php
                                                 $sub_total += $item->amount;
-                                            @endphp
-                                        @endforeach
+                                            ?>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         <tr>
-                                            <td colspan="2" class="text-right">{{ __('Sub Total') }}</td>
-                                            <td class="text-right">Rp {{ number_format($sub_total) }}</td>
+                                            <td colspan="2" class="text-right"><?php echo e(__('Sub Total')); ?></td>
+                                            <td class="text-right">Rp <?php echo e(number_format($sub_total)); ?></td>
                                         </tr>
                                         <tr>
                                             <td colspan="2" class="border-0 text-right">
-                                                <strong>{{ __('Total') }}</strong>
+                                                <strong><?php echo e(__('Total')); ?></strong>
                                             </td>
                                             <td class="border-0 text-right">
-                                                <h4 class="m-0">Rp {{ number_format($sub_total) }}</h4>
+                                                <h4 class="m-0">Rp <?php echo e(number_format($sub_total)); ?></h4>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -162,4 +162,6 @@
             </div>
         </div>
         <!-- end row -->
-    @endsection
+    <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master-layouts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Data\Project\youliantang\resources\views/invoice/view-invoice.blade.php ENDPATH**/ ?>
